d7349a4f45fba0fe8c303886f12809214c35077b
guilherme <guilherme.dinisjunior@gmail.com>
2021-03-28 12:34:58 +0200

    Adds auc metric and arg parsing

--- py/attx/task.py
+++ py/attx/task.py
@@ -5,16 +5,29 @@ from typing import Optional, Callable, Tuple, Dict, Any, List, Generator, List
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.python.ops import lookup_ops
+
 
 from attx import data
 from attx.typedef import FeatureSpec, Tensor
 from attx.typedef import TensorSpec
+import argparse
 
 
 SEQ_LEN = 50
-EMBEDDING_SIZE = 8
 MIN_SEQ_LEN = 3
+
+
+def parse_args() -> argparse.Namespace:
+    arg_parser = argparse.ArgumentParser("Next Token: Supervised Learning")
+    arg_parser.add_argument("--batch-size", type=int, default=64)
+    arg_parser.add_argument("--max-steps", type=int, default=1000000)
+    arg_parser.add_argument(
+        "--model-dir",
+        type=str,
+        default=os.path.join(tempfile.gettempdir(), str(uuid.uuid4())),
+    )
+    args, _ = arg_parser.parse_known_args()
+    return args
 
 
 def create_train_and_eval_input_functions(
@@ -136,28 +149,58 @@ def create_estimator(
 
 
 def create_hooks(log_dir: str) -> List[tf.estimator.SessionRunHook]:
-    step_counter_hook = tf.estimator.StepCounterHook(every_n_steps=100)
+    step_counter_hook = tf.estimator.StepCounterHook(every_n_steps=1000)
     return [step_counter_hook]
 
 
-def train():
+def auc_fn(labels, predictions):
+    auc_metric = tf.keras.metrics.AUC(name="auc")
+
+    def equality(label_class_ids: Tuple[tf.Tensor, tf.Tensor]) -> tf.Tensor:
+        label, class_ids = label_class_ids
+        return tf.equal(label, class_ids)
+
+    indices = tf.where(
+        tf.map_fn(
+            equality,
+            elems=(labels, predictions["all_classes"]),
+            fn_output_signature=tf.bool,
+        )
+    )
+    y_true = tf.one_hot(indices[:, 1], depth=data.VOCAB_SIZE)
+    y_true = tf.squeeze(y_true)
+    auc_metric.update_state(y_true=y_true, y_pred=predictions["probabilities"])
+    return {"auc": auc_metric}
+
+
+def add_metrics(estimator: tf.estimator.Estimator) -> tf.estimator.Estimator:
+    return tf.estimator.add_metrics(estimator, auc_fn)
+
+
+def train(max_steps: int, batch_size: int, model_dir: str):
     # params
-    model_dir = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
-    params = {"layers_config": [8, 8, 8], "embedding_size": 16}
+    params = {"layers_config": [12, 12, 12], "embedding_size": 16}
     config = tf.estimator.RunConfig()
 
     # setup
     estimator = create_estimator(model_dir, config, params)
-    train_input_fn, eval_input_fn = create_train_and_eval_input_functions(SEQ_LEN, 64)
+    estimator = add_metrics(estimator)
+    train_input_fn, eval_input_fn = create_train_and_eval_input_functions(
+        SEQ_LEN, batch_size=batch_size
+    )
 
     hooks = create_hooks(model_dir)
     train_spec = tf.estimator.TrainSpec(
-        input_fn=train_input_fn, hooks=hooks, max_steps=1000
+        input_fn=train_input_fn, hooks=hooks, max_steps=max_steps
     )
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn)
 
+    # train
     tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
 
 
 if __name__ == "__main__":
-    train()
+    args = parse_args()
+    train(
+        max_steps=args.max_steps, batch_size=args.batch_size, model_dir=args.model_dir
+    )
